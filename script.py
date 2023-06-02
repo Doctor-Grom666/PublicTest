15b71ed16487256b29e5afae686fb45fc928dff1
Doctor-Grom666 <127833238+Doctor-Grom666@users.noreply.github.com>
2023-06-02 21:02:49 +0300

    Delete bank

--- script.py
+++
@@ -1,4 +0,0 @@
-per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
-money = float(input('Введите сумму:'))
-deposit = [i * (money / 100) for i in per_cent.values()]
-print(int(max(deposit)))
